c405e5387ab561ed9306d72a574035eefd38bc23
Sereke Asfeday <sereke.asfeday@stu.ocsb.ca>
2026-09-20 22:34:13 +0000

    First commit of hello_world_style.py

---
+++ “hello_world_style.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Created By: Sereke Asfeday
+# Date: Sep. 20th, 2026
+# This program displays greetings and contains proper formatting.
+def main():
+    print("Hello, Sereke!")
+    print("programming is so much fun:)!")
+
+
+if __name__ == "__main__":
+    main()
